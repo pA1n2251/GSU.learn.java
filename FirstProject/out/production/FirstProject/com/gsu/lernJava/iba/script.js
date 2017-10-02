3d87d6bcfdd0ffa6c144b3b536024c18d7ef5b74
Oleg Kameisha <pA1n2251@gmail.com>
2017-10-02 18:41:02 +0300

    class Auto

---
+++ FirstProject/out/production/FirstProject/com/gsu/lernJava/iba/script.js
@@ -0,0 +1,4 @@
+'use strict'
+
+var max = prompt('Введите max');
+alert(Math.random()*max);
---
+++ FirstProject/src/com/gsu/lernJava/Auto.java
@@ -0,0 +1,51 @@
+package com.gsu.lernJava;
+
+public class Auto {
+    private String carModel;
+    private String engineType;
+    private int enginePower;
+    private int mass;
+    private float mileage;
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public int getMass() {
+        return mass;
+    }
+
+    public void setMass(int mass) {
+        this.mass = mass;
+    }
+
+    public float getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(float mileage) {
+        this.mileage = mileage;
+    }
+
+
+}

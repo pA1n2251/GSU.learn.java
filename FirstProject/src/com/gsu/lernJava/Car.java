d1a90cea919470abadb97061ca6771195ac74268
Oleg Kameisha <pA1n2251@gmail.com>
2017-10-05 16:16:04 +0300

    Add Car

---
+++ FirstProject/src/com/gsu/lernJava/Car.java
@@ -0,0 +1,32 @@
+package com.gsu.lernJava;
+
+public class Car extends Auto {
+
+    private float fuelConsumption;
+    private int accelerationTime;
+
+    public Car(){};
+
+    public Car(String carModel, String engineType, int enginePower, int mass, float mileage) {
+        super(carModel, engineType, enginePower, mass, mileage);
+    }
+
+    public Car(String carModel, String engineType, int enginePower, int mass, float mileage, float fuelConsumption, int accelerationTime) {
+        super(carModel, engineType, enginePower, mass, mileage);
+        this.fuelConsumption = fuelConsumption;
+        this.accelerationTime = accelerationTime;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Car{" +
+                "Fuel consumption=" + fuelConsumption +
+                ", acceleration time=" + accelerationTime +
+                '}';
+    }
+
+    @Override
+    public void model(){
+        System.out.println("This is the Car\n");
+    }
+}

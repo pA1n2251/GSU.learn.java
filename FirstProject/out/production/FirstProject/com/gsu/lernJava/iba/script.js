9803185b9bf06c6daeca1cd8f940856eeafc41fb
Oleg Kameisha <pA1n2251@gmail.com>
2017-10-05 15:54:21 +0300

    Change Auto

--- FirstProject/out/production/FirstProject/com/gsu/lernJava/iba/script.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-
-var max = prompt('Введите max');
-alert(Math.random()*max);
--- FirstProject/src/com/gsu/lernJava/Auto.java
+++ FirstProject/src/com/gsu/lernJava/Auto.java
@@ -1,51 +1,58 @@
 package com.gsu.lernJava;
 
-public class Auto {
+abstract public class Auto {
     private String carModel;
     private String engineType;
     private int enginePower;
     private int mass;
     private float mileage;
 
-    public String getCarModel() {
-        return carModel;
+    public Auto(){};
+
+    public Auto(String carModel, String engineType, int enginePower) {
+        this.carModel = carModel;
+        this.engineType = engineType;
+        this.enginePower = enginePower;
     }
 
-    public void setCarModel(String carModel) {
+    public Auto(String carModel, String engineType, int enginePower, int mass, float mileage) {
         this.carModel = carModel;
+        this.engineType = engineType;
+        this.enginePower = enginePower;
+        this.mass = mass;
+        this.mileage = mileage;
+    }
+
+    public String getCarModel() {
+        return carModel;
     }
 
     public String getEngineType() {
         return engineType;
     }
 
-    public void setEngineType(String engineType) {
-        this.engineType = engineType;
-    }
-
     public int getEnginePower() {
         return enginePower;
-    }
-
-    public void setEnginePower(int enginePower) {
-        this.enginePower = enginePower;
     }
 
     public int getMass() {
         return mass;
     }
 
-    public void setMass(int mass) {
-        this.mass = mass;
-    }
-
     public float getMileage() {
         return mileage;
     }
 
-    public void setMileage(float mileage) {
-        this.mileage = mileage;
+    abstract public void model();
+
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "Car model = '" + carModel + '\'' +
+                ", engine type='" + engineType + '\'' +
+                ", engine power=" + enginePower +
+                ", mass=" + mass +
+                ", mileage=" + mileage +
+                '}';
     }
-
-
 }

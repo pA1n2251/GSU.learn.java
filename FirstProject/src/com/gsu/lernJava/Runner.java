c6653bc9dd25f95c5508d554ccd7b70c25f70e67
Oleg Kameisha <pA1n2251@gmail.com>
2017-10-15 19:11:02 +0300

    Finale v1.0

--- FirstProject/src/com/gsu/lernJava/Runner.java
+++ FirstProject/src/com/gsu/lernJava/Runner.java
@@ -1,5 +1,6 @@
 package com.gsu.lernJava;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,6 +11,7 @@ public class Runner {
         Scanner input = new Scanner(System.in);
         System.out.print("Input number of computer: ");
         int counter = input.nextInt();
+
         for (int i = 0; i < counter; i++){
             System.out.print("Input " + (i+1) + " CPU frequency: ");
             float cpuFrequency = input.nextFloat();
@@ -20,10 +22,38 @@ public class Runner {
             Computer temp = new Computer(cpuFrequency, ram, videoCard);
             list.add(temp);
         }
-        for (int i = 0; i < counter; i++) {
-            System.out.println(list.get(i).toString());
+
+        File file = new File("D:\\"+File.separator + "files" + File.separator + "Computer.bin");
+
+        try {
+            FileOutputStream fo = new FileOutputStream(file);
+            ObjectOutputStream oo = new ObjectOutputStream(fo);
+            for (Computer comp: list)
+                System.out.println(comp.toString());
+            oo.writeObject(list);
+            oo.flush();
+            oo.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
 
+        System.out.println("Data has been recorded!");
+        System.out.println("Reading...");
+
+        try {
+            FileInputStream fi = new FileInputStream(file);
+            ObjectInputStream oi = new ObjectInputStream(fi);
+            System.out.println("New list created");
+            ArrayList<Computer> list2 = (ArrayList<Computer>)oi.readObject();
+            for (int i = 0; i < counter; i++) {
+                System.out.println(list2.get(i).toString());
+            }
+            oi.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
 
     }
 }
